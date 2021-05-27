46660013a501ab2247408ea8c0f025a226da2007
Qiao Liu <qiao.liu@iglooinsure.com>
2021-05-27 18:55:03 +0800

    unfinish 450: delete node in binary search tree

---
+++ 450-delete-node-in-bst/delete_node_in_bst.go
@@ -0,0 +1,26 @@
+package deletenodeinbst
+
+type TreeNode struct {
+	val         int
+	left, right *TreeNode
+}
+
+func deleteNodeInBST(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	node := root
+	for node != nil {
+		if node.val == target {
+			var newRoot *TreeNode
+			left := node.left
+			for left != nil {
+				newRoot = node.left
+				left = node.left
+			}
+			if newRoot == nil {
+
+			}
+		}
+	}
+}
