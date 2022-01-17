c25fa3e2596750ae06dd39c90432378208dda50c
Qiao <kentliuqiao@gmail.com>
2022-01-17 11:21:40 +0700

    carry on

--- 450-delete-node-in-bst/delete_node_in_bst.go
+++ 450-delete-node-in-bst/delete_node_in_bst.go
@@ -5,22 +5,22 @@ type TreeNode struct {
 	left, right *TreeNode
 }
 
-func deleteNodeInBST(root *TreeNode, target int) *TreeNode {
-	if root == nil {
-		return nil
-	}
-	node := root
-	for node != nil {
-		if node.val == target {
-			var newRoot *TreeNode
-			left := node.left
-			for left != nil {
-				newRoot = node.left
-				left = node.left
-			}
-			if newRoot == nil {
+// func deleteNodeInBST(root *TreeNode, target int) *TreeNode {
+// 	if root == nil {
+// 		return nil
+// 	}
+// 	node := root
+// 	for node != nil {
+// 		if node.val == target {
+// 			var newRoot *TreeNode
+// 			left := node.left
+// 			for left != nil {
+// 				newRoot = node.left
+// 				left = node.left
+// 			}
+// 			if newRoot == nil {
 
-			}
-		}
-	}
-}
+// 			}
+// 		}
+// 	}
+// }
